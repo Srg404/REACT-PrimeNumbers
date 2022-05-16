a595787fc65cfb0eb5db003bfd6c50e62af14bce
Serge Lecomte <srg404@gmail.com>
2022-05-16 21:09:17 +0200

    Add logic for color

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,8 +4,8 @@ import Header from './components/Header';
 import Keyboard from './components/Keyboard';
 import Menu from './components/Menu';
 import Setting from './components/Setting';
-import { useEffect, useState } from 'react';
 import data from './data/init.json';
+import { useEffect, useState } from 'react';
 import UsePrimeNumber from './hooks/UsePrimeNumber';
 import UseDeepCopy from './hooks/UseDeepCopy';
 
@@ -26,20 +26,18 @@ function App() {
     // generate the Prime Number
     // TODO: Improve this code
     switch (mode) {
-      case 'easy' :
-        setResult(UsePrimeNumber(Math.random().toString().slice(2,7)).slice(-1)[0]);
+      case 'easy':
+        setResult(UsePrimeNumber(Math.random().toString().slice(2, 7)).slice(-1)[0]);
         break;
-      case 'hard' :
-        setResult(UsePrimeNumber(Math.random().toString().slice(2,9)).slice(-1)[0]);
+      case 'hard':
+        setResult(UsePrimeNumber(Math.random().toString().slice(2, 9)).slice(-1)[0]);
         break;
-      default :
-          // "medium"
-          setResult(UsePrimeNumber(Math.random().toString().slice(2,8)).slice(-1)[0]);
-          break;
-
+      default:
+        // 'medium'
+        setResult(UsePrimeNumber(Math.random().toString().slice(2, 8)).slice(-1)[0]);
+        break;
     }
     // Set Board
-    console.log("Reset");
     setBoardArray(UseDeepCopy(data)[mode]);
     gameRound = 0;
     gamePosition = 0;
@@ -52,28 +50,31 @@ function App() {
     (keyVal === "Enter") && checkRow();
   }
 
+  // When the user push on a number key
   function addNum(keyVal) {
-    if (gamePosition < boardArray[0].length){
+    if (gamePosition < boardArray[0].length) {
       const currentRow = boardArray[gameRound];
       currentRow[gamePosition][0] = "neutral";
       currentRow[gamePosition][1] = keyVal;
-      gamePosition = (gamePosition === currentRow.length) ? gamePosition : gamePosition + 1 ;
-      updateBoardRow(currentRow);
+      gamePosition = (gamePosition === currentRow.length) ? gamePosition : gamePosition + 1;
+      updateBoardRow(currentRow, gameRound);
     }
   }
 
+  // When the user push on enter delete
   function removeNum() {
-    if (gamePosition > 0){
+    if (gamePosition > 0) {
       const currentRow = boardArray[gameRound];
       gamePosition--;
       currentRow[gamePosition][0] = "empty";
       currentRow[gamePosition][1] = "0";
-      updateBoardRow(currentRow);
+      updateBoardRow(currentRow, gameRound);
     }
   }
 
-  function updateBoardRow(currentRow) {
-    setBoardArray(boardArray => boardArray.map((el,index) => {
+  // update the board with the new row
+  function updateBoardRow(currentRow, gameRound) {
+    setBoardArray(boardArray => boardArray.map((el, index) => {
       if (gameRound === index) {
         return currentRow;
       }
@@ -81,10 +82,51 @@ function App() {
     }));
   }
 
+  // When the user push on enter key
   function checkRow() {
-    console.log(`Check row`)
+    console.log(gameRound);
+    if ((gameRound < boardArray.length) && (gamePosition === boardArray[0].length)) {
+      console.log(`Check row`);
+
+      const currentRow = boardArray[gameRound];
+      const resultArray = result.toString().split("");
+
+      // Possible Value : empty neutral exist ok 
+
+      const newRow = currentRow.map((el, index) => {
+
+        if (el[1] === resultArray[index]) {
+          return ["ok", el[1]];
+        } else if (resultArray.filter((e) => el[1] === e).length) {
+          return ["exist", el[1]];
+        } else {
+          return el;
+        }
+      });
+
+      updateBoardRow(newRow, gameRound);
+
+      gamePosition = 0;
+      gameRound++;
+
+      if (newRow.every((el) => el[0] === "ok")) {
+        console.log('You win!');
+        gameRound = boardArray.length;
+      }
+    }
+
+    if (gameRound === boardArray.length) {
+      console.log('Game over');
+
+      // init Board
+      setBoardArray(UseDeepCopy(data)[mode]);
+      gameRound = 0;
+      gamePosition = 0;
+      // TODO: generate a new number result
+    }
   }
 
+  // Get the values of keyboard keys
   useEffect(() => {
     document.addEventListener('keyup', handleKeyUp);
     return () => {
@@ -92,14 +134,14 @@ function App() {
     };
   });
 
-  const handleKeyUp = (e) => {
+  function handleKeyUp(e) {
     game(e.key);
   }
 
   return (
     <div className={`App ${theme}`}>
       <header className="App-header">
-        <Header 
+        <Header
           title={title}
           showSetting={showSetting}
           setShowSetting={setShowSetting}
@@ -110,22 +152,22 @@ function App() {
       <main className="App-main">
         <div className="container">
           <center><em>result = <b>{result}</b></em></center>
-          <Board 
-            boardArray={boardArray} 
+          <Board
+            boardArray={boardArray}
             setBoardArray={setBoardArray}
           />
-          <Keyboard 
+          <Keyboard
             game={game}
           />
         </div>
       </main>
-      <Menu 
+      <Menu
         showMenu={showMenu}
       />
-      <Setting 
-        showSetting={showSetting} 
+      <Setting
+        showSetting={showSetting}
         setTheme={setTheme}
-        mode= {mode}
+        mode={mode}
         setMode={setMode}
       />
     </div>
